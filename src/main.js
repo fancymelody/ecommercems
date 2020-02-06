8275f3e67117d5b1ffc5fecb5eca31c562652e44
fancymelody <2769501950@qq.com>
2020-02-06 20:46:51 +0800

    主页构建（左边侧边栏）

--- src/main.js
+++ src/main.js
@@ -17,6 +17,13 @@ Vue.use(ElementUI);
 
 // Vue.prototype.axios = axios;
 axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/'
+// 通过axios请求拦截器添加token,保证拥有获取数据的权限
+axios.interceptors.request.use(config=>{
+  // console.log(config)
+  config.headers.Authorization=window.sessionStorage.getItem('token')
+  // console.log(config)
+  return config;
+})
 Vue.prototype.$http=axios
 
 /* eslint-disable no-new */
