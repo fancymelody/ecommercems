e45e2c8ba8bae486077c5834ac1cccf69662ac14
fancymelody <2769501950@qq.com>
2020-02-08 22:34:40 +0800

    删除的小bug

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -256,25 +256,26 @@
             async deleteUser(id) {
                 console.log("删除用户id")
                 console.log(id)
-                const confirmResult=await this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
+                const confirmResult = await this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
                     confirmButtonText: '确定',
                     cancelButtonText: '取消',
                     type: 'warning'
-                }).catch(err=>err)
+                }).catch(err => err)
                 // console.log(confirmResult)
-                if(confirmResult!=="confirm"){
+                if (confirmResult === "confirm") {
+                    this.$http
+                        .delete('users/' + id)
+                        .then(response => {
+                            console.log(response)
+                            this.$message.success("删除成功")
+                            this.getUserList()
+                        })
+                        .catch(err => {
+                            this.$message.error("删除失败")
+                        })
+                } else {
                     this.$message.info('已取消删除')
                 }
-                this.$http
-                .delete('users/'+id)
-                .then(response=>{
-                    console.log(response)
-                    this.$message.success("删除成功")
-                    this.getUserList()
-                })
-                .catch(err=>{
-                    this.$message.error("删除失败")
-                })
             }
         }
     }
