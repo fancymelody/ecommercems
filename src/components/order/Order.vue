91ffda11c2175590bd7f5bfa9c5aa37b243e73ac
fancymelody <2769501950@qq.com>
2020-02-13 11:54:46 +0800

    订单列表功能实现

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -35,7 +35,7 @@
                 <el-table-column label="操作">
                     <template slot-scope="scope">
                         <el-button type="primary" size="mini" icon="el-icon-edit" @click="editOrder"></el-button>
-                        <el-button type="success" size="mini" icon="el-icon-location"></el-button>
+                        <el-button type="success" size="mini" icon="el-icon-location" @click="showProgess"></el-button>
                     </template>
                 </el-table-column>
             </el-table>
@@ -45,10 +45,35 @@
                 layout="total, sizes, prev, pager, next, jumper" :total="total">
             </el-pagination>
         </el-card>
+        <!-- 编辑订单对话框 -->
+        <el-dialog title="修改地址" :visible.sync="editDialogVisible" width="50%" @close="editDialogClosed">
+            <el-form :model="editForm" :rules="editFormRules" ref="editFormRef" label-width="100px">
+                <el-form-item label="省市区/县" prop="address1">
+                    <el-cascader :options="cityData" v-model="editForm.address1"></el-cascader>
+                </el-form-item>
+                <el-form-item label="详细地址" prop="address2">
+                    <el-input v-model="editForm.address2"></el-input>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="editDialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="editDialogVisible = false">确 定</el-button>
+            </span>
+        </el-dialog>
+        <!-- 展示物流进度对话框 -->
+        <el-dialog title="物流进度" :visible.sync="progressDialogVisible" width="50%">
+            <el-timeline>
+                <el-timeline-item v-for="(item, index) in progressInfo" :key="index" :timestamp="item.time">
+                    {{item.context}}
+                </el-timeline-item>
+            </el-timeline>
+        </el-dialog>
     </div>
 </template>
 
 <script>
+    import cityData from './citydata.js'
+
     export default {
         data() {
             return {
@@ -58,7 +83,83 @@
                     pagesize: 10
                 },
                 total: 0,
-                orderList: []
+                orderList: [],
+                editDialogVisible: false,
+                editForm: {
+                    address1: [],
+                    address2: ''
+                },
+                editFormRules: {
+                    address1: [
+                        { required: true, message: '请输入选择省市区/县', trigger: 'blur' }
+                    ],
+                    address2: [
+                        { required: true, message: '请输入详细地址', trigger: 'blur' }
+                    ]
+                },
+                cityData: cityData,
+                progressDialogVisible: false,
+                progressInfo: [
+                    {
+                        "time": "2018-05-10 09:39:00",
+                        "ftime": "2018-05-10 09:39:00",
+                        "context": "已签收,感谢使用顺丰,期待再次为您服务",
+                        "location": ""
+                    },
+                    {
+                        "time": "2018-05-10 08:23:00",
+                        "ftime": "2018-05-10 08:23:00",
+                        "context": "[北京市]北京海淀育新小区营业点派件员 顺丰速运 95338正在为您派件",
+                        "location": ""
+                    },
+                    {
+                        "time": "2018-05-10 07:32:00",
+                        "ftime": "2018-05-10 07:32:00",
+                        "context": "快件到达 [北京海淀育新小区营业点]",
+                        "location": ""
+                    },
+                    {
+                        "time": "2018-05-10 02:03:00",
+                        "ftime": "2018-05-10 02:03:00",
+                        "context": "快件在[北京顺义集散中心]已装车,准备发往 [北京海淀育新小区营业点]",
+                        "location": ""
+                    },
+                    {
+                        "time": "2018-05-09 23:05:00",
+                        "ftime": "2018-05-09 23:05:00",
+                        "context": "快件到达 [北京顺义集散中心]",
+                        "location": ""
+                    },
+                    {
+                        "time": "2018-05-09 21:21:00",
+                        "ftime": "2018-05-09 21:21:00",
+                        "context": "快件在[北京宝胜营业点]已装车,准备发往 [北京顺义集散中心]",
+                        "location": ""
+                    },
+                    {
+                        "time": "2018-05-09 13:07:00",
+                        "ftime": "2018-05-09 13:07:00",
+                        "context": "顺丰速运 已收取快件",
+                        "location": ""
+                    },
+                    {
+                        "time": "2018-05-09 12:25:03",
+                        "ftime": "2018-05-09 12:25:03",
+                        "context": "卖家发货",
+                        "location": ""
+                    },
+                    {
+                        "time": "2018-05-09 12:22:24",
+                        "ftime": "2018-05-09 12:22:24",
+                        "context": "您的订单将由HLA（北京海淀区清河中街店）门店安排发货。",
+                        "location": ""
+                    },
+                    {
+                        "time": "2018-05-08 21:36:04",
+                        "ftime": "2018-05-08 21:36:04",
+                        "context": "商品已经下单",
+                        "location": ""
+                    }]
             }
         },
         created() {
@@ -89,12 +190,32 @@
                 this.getOrderList()
             },
             editOrder() {
-
+                this.editDialogVisible = true
+            },
+            editDialogClosed() {
+                this.$refs.editFormRef.resetFields()
+            },
+            showProgess() {
+                // 后台API 1.10.5有问题
+                // 804909574412544580
+                // this.$http
+                // .get('/kuaidi/1106975712662')
+                // .then(res=>{
+                //     console.log(res.data)
+                //     // this.progressInfo=res.data.data.goods
+                // })
+                // .catch(err=>{
+                //     console.log(err)
+                //     this.$message.error('获取订单失败')
+                // })
+                this.progressDialogVisible = true
             }
         }
     }
 </script>
 
 <style scoped>
-
+    .el-cascader {
+        width: 100%;
+    }
 </style>
