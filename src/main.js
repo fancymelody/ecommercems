4e6d2db7cb1ac335c04d66a49ce2d310e2c6c7a8
fancymelody <2769501950@qq.com>
2020-02-13 17:51:05 +0800

    添加进度条

--- src/main.js
+++ src/main.js
@@ -14,40 +14,51 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 // 导入vue-quill-editor富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
-// require styles
+// 导入vue-quill-editor的样式
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
+// 导入nprogress插件
+import NProgress from 'nprogress'
+// 导入nprogress对应的css
+import 'nprogress/nprogress.css'
 
 Vue.use(VueQuillEditor)
 
 Vue.config.productionTip = false
 
-Vue.component('tree-table',ZkTable)
+Vue.component('tree-table', ZkTable)
 Vue.use(ElementUI);
 
 // 时间过滤器
-Vue.filter('dataFormat',function(originVal){
-  const dt=new Date(originVal)
-  const y=dt.getFullYear()
-  const m=(dt.getMonth()+1+'').padStart(2,'0')
-  const d=(dt.getDate()+'').padStart(2,'0')
-  const hh=(dt.getHours()+'').padStart(2,'0')
-  const mm=(dt.getMinutes()+'').padStart(2,'0')
-  const ss=(dt.getSeconds()+'').padStart(2,'0')
-  return y+'-'+m+'-'+d+' '+hh+':'+mm+':'+ss
+Vue.filter('dataFormat', function (originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
 })
 
 // Vue.prototype.axios = axios;
-axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // 通过axios请求拦截器添加token,保证拥有获取数据的权限
-axios.interceptors.request.use(config=>{
+// 在request拦截器中,展示进度条 NProgress.start();
+axios.interceptors.request.use(config => {
   // console.log(config)
-  config.headers.Authorization=window.sessionStorage.getItem('token')
+  NProgress.start()
+  config.headers.Authorization = window.sessionStorage.getItem('token')
   // console.log(config)
   return config;
 })
-Vue.prototype.$http=axios
+// 在response拦截器中,展示进度条 NProgress.done();
+axios.interceptors.response.use(config => {
+  NProgress.done();
+  return config;
+})
+Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
 new Vue({
